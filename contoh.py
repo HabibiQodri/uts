2201f0c05b08bfbc2bdfcc7f3d0d76f9526c7112
Muhammad Habibi Ferbiyon Qodri <habibiqodri0511@gmail.com>
2022-04-21 15:35:47 +0700

    Update contoh.py

--- contoh.py
+++ contoh.py
@@ -1,1 +1,15 @@
-print("hello world")
+# looping = [10,20,30,40,50,60,70]
+# #contoh perulangan/looping 
+# for i in looping:
+#     print(f"i -> {i}")
+# loopingBerkondisi = [10,20,30,40,50,60,70]
+
+# contoh perulangan/looping dengan range
+# for i in range(1,5):
+#     print("syaa ganteng")
+
+# contoh perulangan/looping dengan tipe data string
+# data_str = "saya ganteng banget"
+# for huruf in data_str:
+#     print(huruf)
+    
