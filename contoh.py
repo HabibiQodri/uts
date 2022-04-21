35558794e2af5a67818b2e3f73478c613e250e7a
Muhammad Habibi Ferbiyon Qodri <habibiqodri0511@gmail.com>
2022-04-21 15:45:24 +0700

    Update contoh.py

--- contoh.py
+++ contoh.py
@@ -1,15 +1,20 @@
-# looping = [10,20,30,40,50,60,70]
-# #contoh perulangan/looping 
-# for i in looping:
-#     print(f"i -> {i}")
-# loopingBerkondisi = [10,20,30,40,50,60,70]
 
-# contoh perulangan/looping dengan range
-# for i in range(1,5):
-#     print("syaa ganteng")
+print("Banyak bilangna genap yang akan ditampilkan (dimulai dari 2) : ")
+bilangan = int(input("Masukkan banyak bilangan = "))
 
-# contoh perulangan/looping dengan tipe data string
-# data_str = "saya ganteng banget"
-# for huruf in data_str:
-#     print(huruf)
-    
+bilagan_genap = 2
+i = 1
+
+while i <= bilangan:
+    i += 1
+    bilagan_genap += 2
+    # menghentikan perulangan jika bilangan lebih dari 20
+    if bilagan_genap > 50:
+        print("Perulangan dihentikan karena melewati batas jumlah bilangan")
+        break
+
+    # menampilkan string pada kondisi bilangan = 10
+    if bilagan_genap == 10:
+        print("Anda Telah Sampai Pada Bilangan 10 ")
+        continue
+    print(f"{bilagan_genap} ")
